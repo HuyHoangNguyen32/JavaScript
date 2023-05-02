633821ba2f0d47910eb827ac2ed76bf57d1d1b79
HuyHoangNguyen32 <92217902+HuyHoangNguyen32@users.noreply.github.com>
2023-05-02 12:29:56 +0900

    🎉 Section 06 Random Number [0,n]

---
+++ Section6:Number/06-06-main.js
@@ -0,0 +1,22 @@
+// random a number in range of [0, n]
+// Math.random() => [0, 1)
+
+function randomNumber(n) {
+  if (n <= 0) return;
+
+  const random = Math.random() * n;
+  const result = Math.round(random);
+
+  return result;
+}
+
+console.log(randomNumber(3));
+console.log(randomNumber(3));
+console.log(randomNumber(3));
+console.log(randomNumber(3));
+console.log(randomNumber(3));
+console.log(randomNumber(3));
+console.log(randomNumber(3));
+console.log(randomNumber(3));
+console.log(randomNumber(3));
+console.log(randomNumber(3));
