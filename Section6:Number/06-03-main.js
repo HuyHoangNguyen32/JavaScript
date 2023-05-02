7e67529920c67f370d4491067b5f82dda321c38a
HuyHoangNguyen32 <92217902+HuyHoangNguyen32@users.noreply.github.com>
2023-05-02 11:35:32 +0900

    🎉 Section 6 Built-in object Math

---
+++ Section6:Number/06-03-main.js
@@ -0,0 +1,19 @@
+
+console.log(Math.PI) // 3.141592653589793
+
+const a = -123.525;
+
+console.log(Math.ceil(a)); // -124
+console.log(Math.floor(a)); // -123
+
+console.log(Math.round(a)) // -124
+
+console.log(Math.trunc(a)) // -123
+
+console.log(Math.random()) // 0.10490166563029901
+
+console.log(Math.abs(a)) // 123.525
+
+console.log(Math.pow(2,3)) // 8
+
+console.log(Math.sqrt(81)) // 9
